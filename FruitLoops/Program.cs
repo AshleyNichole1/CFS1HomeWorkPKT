739b5272e59c1180ee08ccc5c0a610f04f76103f
Ashley Smith <AshleyNicholeLea@outlook.com>
2021-07-25 15:37:53 -0500

    FruitLoop Lab Complete

---
+++ FruitLoops/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Fruit = new string[6];
+            Fruit[0] = "Apple";
+            Fruit[1] = "Orange";
+            Fruit[2] = "Pear";
+            Fruit[3] = "Kiwi";
+            Fruit[4] = "Banana";
+            Fruit[5] = "Grape";
+
+            for (int FruitNames = 0; FruitNames < 6; FruitNames++)
+            {
+                Console.Write(Fruit[FruitNames] + ".");
+
+
+
+                for (int J = 0; J < FruitNames; J++)
+                {
+
+                    Console.Write(".");
+
+                }
+                Console.WriteLine();
+
+            }//END FOR
+
+
+
+
+
+
+
+
+
+        }//END MAIN
+    }//END CLASS
+}//END NAMESPACE
